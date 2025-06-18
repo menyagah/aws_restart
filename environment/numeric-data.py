8eb26900e8325fcbcec90f5f91698a07f05eef91
martin <martin@menyagah27@gmail.com>
2025-06-18 22:09:27 +0300

    Ajira aws restart assignment task- Learning python fundamentals

---
+++ environment/numeric-data.py
@@ -0,0 +1,29 @@
+myValue=1
+myValue=3.14
+
+myValue=5j
+
+myValue=True
+myValue=False
+
+
+print(myValue)
+print(type(myValue))
+
+
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("Python has three numeric types: int, float, and complex")
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
